c1af6e92d4c64ce0ada339fc6be62efeba15fce3
alphabetinstitute <165389840+alphabetinstitute@users.noreply.github.com>
2024-04-17 18:59:34 +0530

    hasmode to resolve refresh issue

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,35 @@
-import { createWebHistory, createRouter } from "vue-router"; 
+import { createWebHashHistory, createRouter } from "vue-router";
 import Home from"../components/Home.vue";
 import LoginPage from "../components/LoginPage.vue";
 import AttendancePage from "../components/Attendance.vue";
 import AttendanceView from "../components/AttendanceView.vue";
-const routes =[ 
-    { 
-        path: "/", 
-        name: "HomePage", 
-        component: Home, 
-    },  
-    { 
-        path: "/Login", 
-        name: "LoginPage", 
-        component: LoginPage,
-    }, 
-    { 
-        path: "/attendance", 
-        name: "AttendancePage", 
-        component: AttendancePage,
-    }, 
-    { 
-        path: "/attendanceview", 
-        name: "AttendanceView", 
-        component: AttendanceView,
-    }, 
-]; 
 
-const router = createRouter({ history: createWebHistory(), routes, });
+const routes = [
+    {
+        path: "/",
+        name: "HomePage",
+        component: Home,
+    },
+    {
+        path: "/login",
+        name: "LoginPage",
+        component: LoginPage,
+    },
+    {
+        path: "/attendance",
+        name: "AttendancePage",
+        component: AttendancePage,
+    },
+    {
+        path: "/attendanceview",
+        name: "AttendanceView",
+        component: AttendanceView,
+    },
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+});
+
 export default router;
